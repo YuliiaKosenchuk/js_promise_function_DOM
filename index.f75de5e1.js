8544e6840ad689a95130d3406c26281a95c6ffaf
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2024-08-11 20:41:16 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,29 @@
+"use strict";
+function waitFor(element, eventName) {
+    return new Promise((resolve)=>{
+        element.addEventListener(eventName, (e)=>{
+            if (e.target === element) resolve(`It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`);
+        });
+    });
+}
+const printMessage = (message)=>{
+    const newDiv = document.createElement("div");
+    newDiv.className = "message";
+    newDiv.textContent = message;
+    newDiv.style.position = "fixed";
+    newDiv.style.width = "400px";
+    document.body.append(newDiv);
+};
+const loginField = document.getElementById("login");
+const passwordField = document.getElementById("password");
+const button = document.getElementById("submit");
+waitFor(loginField, "click").then(printMessage);
+waitFor(passwordField, "click").then(printMessage);
+waitFor(button, "click").then(printMessage);
+waitFor(loginField, "input").then(printMessage);
+waitFor(passwordField, "input").then(printMessage);
+waitFor(loginField, "blur").then(printMessage);
+waitFor(passwordField, "blur").then(printMessage);
+waitFor(button, "blur").then(printMessage);
+
+//# sourceMappingURL=index.f75de5e1.js.map
